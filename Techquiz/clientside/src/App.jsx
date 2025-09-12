3c26cd1b80e257ca3d179d514fc030d9523854a1
Aakash4578 <137828737+Aakash4578@users.noreply.github.com>
2025-09-12 17:21:36 +0500

    feat: added new password show/hide

--- Techquiz/clientside/src/App.jsx
+++ Techquiz/clientside/src/App.jsx
@@ -39,8 +39,8 @@ import Vieworders from "./Components/AdminPanel/Vieworders";
 import Orderdetails from "./Components/website/Orderdetails";
 import Mypet from "./Components/website/Mypet";
 import Add_pet from "./Components/website/Add_pet";
-import Loginforshelterandvetes from "./Components/Auth/Loginforshelterandvetes"
-import VetesprotectedRoutes from "./Components/vetesPanels/"
+import Loginforshelterandvetes from "./Components/Auth/Loginforshelterandvetes";
+import VetesprotectedRoutes from "./Components/vetesPanels/";
 import VetesProfile from "./Components/vetesPanels/VetesProfile";
 
 import Veteindex from "./Components/vetesPanels/Veteindex"
