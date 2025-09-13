ca8e8a63a073586c8fe01a0f72fabe89a799e803
Aakash4578 <137828737+Aakash4578@users.noreply.github.com>
2025-09-13 18:24:42 +0500

    feat: ok

--- Techquiz/clientside/src/Components/website/Home.jsx
+++ Techquiz/clientside/src/Components/website/Home.jsx
@@ -49,7 +49,97 @@ function Home() {
 				</div>
 			</section>
 				
+	<section class="about-section border-rad space-20 mt-20 mb-20 pt-180 pb-120 pt-lg-60 pb-lg-10"
+				data-bg-color="#E7D8FF">
+				<div class="container custom-container">
+					<div class="row align-items-center">
+						<div class="col-lg-6 mb-45">
+							<div class="img-wrapper-eight pe-xxl-5 me-xxl-4 position-relative">
+								<img class="main-img img-fluid" src="assets/img/media/media-50.png" alt="About"/>
+								<div class="float-content-5 jump d-none d-md-inline-block">
+									<img class="img-shape-2 position-relative img-fluid"
+										src="assets/img/shape/shape-27.png" alt="shape"/>
+									<div class="call-box d-flex align-items-center position-absolute">
+										<div class="icon">
+											<img src="assets/img/icon/icon-5.svg" alt="icon"/>
+										</div>
+										<div class="call-content">
+											<h4 class="call-title">Call Us Anytime</h4>
+											<h4 class="call-number"><a href="#">+86-085-550-1539</a></h4>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-6 ps-xxl-4 wow fadeInRight mb-45">
+							<div class="title-two mb-20">
+								<h2 class="title">Trusted provider of pet care</h2>
+								<p>The top agency offering exceptional services to cater to every aspect of your pet’s
+									care and comfort.</p>
+							</div>
+							<div class="faq-que-list faq-style-3 mb-90">
+								<div class="accordion accordion-one" id="accordion5">
+									<div class="accordion-item">
+										<h2 class="accordion-header" id="heading5s">
+											<button class="accordion-button collapsed" type="button"
+												data-bs-toggle="collapse" data-bs-target="#collapse5s"
+												aria-expanded="false" aria-controls="collapse5s">
+												Who We Are
+											</button>
+										</h2>
+										<div id="collapse5s" class="accordion-collapse collapse"
+											aria-labelledby="heading5s" data-bs-parent="#accordion5">
+											<div class="accordion-body">
+												<p>We are passionate pet caregivers, providing love, safety, and expert
+													care to ensure your pets stay happy, healthy, and comfortable.</p>
+											</div>
+										</div>
+									</div>
+									<div class="accordion-item">
+										<h2 class="accordion-header" id="headingTwo5s">
+											<button class="accordion-button collapsed" type="button"
+												data-bs-toggle="collapse" data-bs-target="#collapseTwo5s"
+												aria-expanded="false" aria-controls="collapseTwo5s">
+												Customize Attention & care
+											</button>
+										</h2>
+										<div id="collapseTwo5s" class="accordion-collapse collapse"
+											aria-labelledby="headingTwo5s" data-bs-parent="#accordion5">
+											<div class="accordion-body">
+												<p>We are passionate pet caregivers, providing love, safety, and expert
+													care to ensure your pets stay happy, healthy, and comfortable.</p>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
 
+							<a class="ht-btn style-13" href="about.html">more About Us <span class="btn-icon"><img
+										src="assets/img/icon/arrow-8.svg" alt="arrow"/></span></a>
+						</div>
+					</div>
+					<div class="mt-65 row row-cols-lg-3 row-cols-md-3 row-cols-1">
+						<div class="col">
+							<div class="counter-wrap-4 text-md-start text-center">
+								<h3 class="number"><span class="counter">15</span>+</h3>
+								<p class="counter-title">Years of experience</p>
+							</div>
+						</div>
+						<div class="col d-md-flex justify-content-center text-center">
+							<div class="counter-wrap-4">
+								<h3 class="number"><span class="counter">75</span>k+</h3>
+								<p class="counter-title">Adopted Animals</p>
+							</div>
+						</div>
+						<div class="col d-md-flex justify-content-md-end justify-content-center">
+							<div class="counter-wrap-4">
+								<h3 class="number"><span class="counter">150</span>+</h3>
+								<p class="counter-title">Total Professional Experts</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
 		</main>
 <Footer/>
 
